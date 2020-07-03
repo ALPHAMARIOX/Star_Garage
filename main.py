c3d39b50b0e9252ab07f5b008092c31eddfc1adc
Lee Gordon Joseph Harrold <alphamariox@gmail.com>
2020-07-02 21:17:33 -0600

    Linked UI to Main

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 import star
+import sys
+
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+
+class StarGarage(QtWidgets.QMainWindow):
+	
+	def __init__(self):
+		
+		super(StarGarage,self).__init__()
+		uic.loadUi('star_garage.ui',self)
+		
+	def run(self):
+		self.mainwindow.mainloop()
+	
+if __name__ == '__main__':
+	app = QtWidgets.QApplication(sys.argv)
+	window = StarGarage()
+	window.show()
+	sys.exit(app.exec_())
--- star.py
+++ star.py
@@ -2,22 +2,30 @@
 ##### Offsets for stats of the stars. Some may have more variables linked to it than others. ######
 
 #Base Weight of Star
-WGT = 0x3C
+WGT = 0x2B8
+KB_ENDURANCE = 0x68
+
 weight = 0
+knockback_e = 0
 
 #Base HP of Star
 HP = 0x9C
+
 health = 0
 
 #Base Speed of Star
 GSPD = 0xC0
-spd = 0
 SUPSPD = 0xC4
 SDOWNSPD = 0xC8
-slopeu = 0
-sloped = 0
 STRTSPD = 0x16C
 ACCELAIR = 0x17C
+SPDTAKEOFF = 0x16C
+
+spd = 0
+slopeu = 0
+sloped = 0
+accair = 0
+startspd = 0
 
 #Base Charge.
 CHRGSPD = 0xCC
@@ -25,18 +33,34 @@ CHRGSPDTRN = 0xD0
 CHRGDEP = 0xD4
 CHRGAIR = 0x190
 
+chargespeed = 0
+chargespeed_t = 0
+charge_dep = 0
+chargespeed_g = 0
+
 #Base Boost.
 BOOSTNONE = 0xD8
 BOOSTHALF = 0xDC
 BOOSTFULL = 0xE0
-#BOOSTCHRG = 0x10C
+BOOSTSLD = 0x10C
 
-#Base Turning.
+boost_n = 0
+boost_h = 0
+boost_f = 0
+boost_s = 0
+
+#Base Turning. 0x110 will not affect the Swerve Star.
 TURNNORMAL = 0x110
 TURNAIR = 0x180
 TRNSPDRAILS = 0x1F0
 TRNLOWSPD = 0x27C
 TRNHIGHSPD = 0x280
+
+turn = 0
+turn_a = 0
+turn_r = 0
+turn_l = 0
+turn_h = 0
 
 #SPDNOSEUP = 0x1A4
 #AMTNOSEUP = 0x1A8
@@ -47,11 +71,31 @@ TRNHIGHSPD = 0x280
 #SPDAIRSTRGHT = 0x1C0
 #SPDNONTURNAIRX = 0x1C8
 
-#HOVER = 0x220
-
 #Base Glide.
 GLIDE = 0x338
 
+glide = 0
+
+#Base Offense
+OFFENSE = 0x3C
+
+offense = 0
+
+#Base Defense
+DEFENSE = 0xBC
+
+defense = 0
+
 #Miscellaneous stats
-BOUNCE = 0x2FC
+BOUNCE = 0x270
+#GPULL = 0x2FC
 GRAVITY = 0x328
+#HITBOX = 0xA0
+#HITBOXK = 0xA4
+#HITBOXC = 0x14C
+#HITBOXKC = 0x150
+#QSTORNSIZE = 0x164
+#GRNDDIS = 0x220
+
+bounce = 0
+gravity = 0
